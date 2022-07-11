8669bcb32ff2d3a6f124dab238a3bf456f0ff09a
Leon Linhart <themrmilchmann@gmail.com>
2022-07-11 23:08:20 +0200

    feat: migrate to Nuxt 2 + Vue 2
    It just works.ö

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,17 @@
-import { defineNuxtConfig } from "nuxt";
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-    build: {
-        transpile: [ "vuetify" ]
-    },
-
-    css: [
-        "vuetify/lib/styles/main.sass"
+export default {
+    buildModules: [
+        "@nuxtjs/vuetify"
     ],
 
-    nitro: {
-        preset: "service-worker"
+    components: true,
+
+    head: {
+        title: "GW2TB Link"
     },
 
     typescript: {
         strict: true,
         typeCheck: true
-    },
-
-    vite: {
-        define: {
-            "process.env.DEBUG": false
-        }
     }
-});
+};
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      Hello World
+    </v-main>
+
+    <v-footer>
+      Hello World
+    </v-footer>
+  </v-app>
+</template>
+
+<style>
+html {
+  overflow: auto;
+}
+</style>
