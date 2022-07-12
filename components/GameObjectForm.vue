25277f8c2f34e0c2188a893247cb553ecc3a976a
Leon Linhart <themrmilchmann@gmail.com>
2022-07-12 21:38:27 +0200

    feat: add basic object form component

---
+++ components/GameObjectForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="d-flex align-center">
+    <a :href="itemUrl" style="margin-right: 16px">
+      <v-img
+        class="elevation-2"
+        max-height="48px"
+        max-width="48px"
+        :src="iconSrc"
+      />
+    </a>
+
+    <v-combobox
+      v-model="modelValue"
+      :items="items"
+      :label="label"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    label: { type: String, required: true },
+    iconSrc: { type: String, required: true },
+    items: Array,
+    itemUrl: { type: String, required: true},
+    required: Boolean,
+    value: { required: true }
+  },
+  computed: {
+    modelValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  }
+});
+</script>
