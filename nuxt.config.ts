9fcd34b56e4bef100d3aabe2d9c49a9caad7eec4
Leon Linhart <themrmilchmann@gmail.com>
2022-07-12 16:18:29 +0200

    feat: resolve all paths to the main page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,15 @@ export default {
 
     head: {
         title: "GW2TB Link"
+    },
+
+    router: {
+        extendRoutes(routes, resolve) {
+            routes.push({
+                name: "catchall",
+                path: "*",
+                component: resolve(__dirname, "pages/index.vue")
+            })
+        }
     }
 };
